2e3239072d5278b5e7eb629643498812102c227a
Yann <yann.domingo@gmail.com>
2021-05-30 19:09:13 +0100

    Fix linting errors

--- src/scripts/dispatch.sh
+++ src/scripts/dispatch.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 CheckEnv() {
   if [ -z "${GH_TOKEN:-}" ]; then
     echo "In order to use the Config dispatcher orb, an OAuth token must be present via the GH_TOKEN environment variable."
@@ -5,12 +6,12 @@ CheckEnv() {
     exit 1
   fi
 
-  if [ ! -s ${TARGETS_LIST} ]; then
+  if [ ! -s "${TARGETS_LIST}" ]; then
     echo "Target file is empty. Please add at least 1 target repository"
     exit 1
   fi
 
-  if [ $(git hash-object ${CENTRAL}) = $(git hash-object $HOME/project/.circleci/config.yml) ]; then
+  if [ "$(git hash-object "${CENTRAL}")" = "$(git hash-object "$HOME"/project/.circleci/config.yml)" ]; then
     echo "The configuratio file you're deploying can't be the same as this build's confguration file"
     echo "Check the content of the configuration file you're trying to deploy"
     exit 1
@@ -27,29 +28,29 @@ CheckEnv() {
 
 DispatchConf() {
   jq -c < "${TARGETS_LIST}" > COMPACT.JSON
-  GLOBAL_64="$(cat ${CENTRAL} | base64 -w 0)"
+  GLOBAL_64=$(base64 -w 0 < "${CENTRAL}")
 while read -r target
   do
     REPO=$(echo "$target" | jq -r '."target-repo"')
     BRANCH=$(echo "$target" | jq -r '."target-branch"')
 
-    if [ $(curl -s -w '%{response_code}' --output /dev/null --location --request GET "https://api.github.com/repos/$REPO/contents/.circleci/config.yml?ref=$BRANCH" --header "Authorization: token $GH_TOKEN" --header "Accept: application/vnd.github.v3+json") != 200 ]; then
-      curl -s -w '%{response_code}' -X PUT https://api.github.com/repos/$REPO/contents/.circleci/config.yml -H "Authorization: token $GH_TOKEN" -H 'Accept: application/vnd.github.v3+json' -d '{"message":"'"$MESSAGE"'","content":"'"$GLOBAL_64"'","branch":"'"$BRANCH"'"}' > /tmp/response.status
+    if [ "$(curl -s -w '%{response_code}' --output /dev/null --location --request GET https://api.github.com/repos/"$REPO"/contents/.circleci/config.yml?ref="$BRANCH" --header "Authorization: token $GH_TOKEN" --header "Accept: application/vnd.github.v3+json")" != "200" ]; then
+      curl -s -w '%{response_code}' -X PUT https://api.github.com/repos/"$REPO"/contents/.circleci/config.yml -H "Authorization: token $GH_TOKEN" -H 'Accept: application/vnd.github.v3+json' -d '{"message":"'"$MESSAGE"'","content":"'"$GLOBAL_64"'","branch":"'"$BRANCH"'"}' > /tmp/response.status
 
       if [[ "$(cat /tmp/response.status)" = "200" ]]; then
-        printf "\n${CENTRAL} was successfully deployed to $REPO [branch: $BRANCH\n"
+        printf "\n%s was successfully deployed to %s [branch: %s\n" "${CENTRAL}" "$REPO" "$BRANCH"
       fi
     else
-      BLOB_SHA=$(curl -s --location --request GET "https://api.github.com/repos/${REPO}/contents/.circleci/config.yml?ref=${BRANCH}" --header "Authorization: token ${GH_TOKEN}" --header "Accept: application/vnd.github.v3+json"|jq -r .sha)
+      BLOB_SHA=$(curl -s --location --request GET https://api.github.com/repos/"${REPO}"/contents/.circleci/config.yml?ref="${BRANCH}" --header "Authorization: token $GH_TOKEN" --header "Accept: application/vnd.github.v3+json"|jq -r .sha)
 
-      if [ $(git hash-object ${CENTRAL}) != $BLOB_SHA ] || [ "${FORCE}" == "1" ]; then
-        curl -s -o /dev/null -w '%{response_code}' -X PUT https://api.github.com/repos/$REPO/contents/.circleci/config.yml --header "Authorization: token ${GH_TOKEN}" --header "Accept: application/vnd.github.v3+json" --data-raw '{"message":"'"$MESSAGE"'","content":"'"${GLOBAL_64}"'","sha": "'"$BLOB_SHA"'","branch":"'"$BRANCH"'"}' > /tmp/response.status
+      if [ "$(git hash-object "${CENTRAL}")" != "$BLOB_SHA" ] || [ "${FORCE}" == "1" ]; then
+        curl -s -o /dev/null -w '%{response_code}' -X PUT https://api.github.com/repos/"$REPO"/contents/.circleci/config.yml --header "Authorization: token $GH_TOKEN" --header "Accept: application/vnd.github.v3+json" --data-raw '{"message":"'"$MESSAGE"'","content":"'"${GLOBAL_64}"'","sha": "'"$BLOB_SHA"'","branch":"'"$BRANCH"'"}' > /tmp/response.status
                         
         if [[ "$(cat /tmp/response.status)" = "200" ]]; then
-          printf "\n${CENTRAL} was successfully deployed to $REPO [branch: $BRANCH]\n"
+          printf "\n%s was successfully deployed to %s [branch: %s\n" "${CENTRAL}" "$REPO" "$BRANCH"
         fi
       else
-        printf "\nThe same version of the configuration file already exists in [branch: $BRANCH]\n"
+        printf "\nThe same version of the configuration file already exists in %s [branch: %s]\n" "$REPO" "$BRANCH"
       fi
     fi  
   done < COMPACT.JSON
